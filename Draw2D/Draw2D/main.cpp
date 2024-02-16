4a13341569212d2173afa4e0b36365723c1e1a6e
Zy Yoooung <yoooung@zydemacbook-pro.lan>
2024-02-15 23:04:48 -0500

    finish two objects

--- Draw2D/Draw2D/main.cpp
+++ Draw2D/Draw2D/main.cpp
@@ -40,26 +40,41 @@ const int VIEWPORT_X = 0,
 const char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
            F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 
+const float MILLISECONDS_IN_SECOND = 1000.0;
+const float DEGREES_PER_SECOND = 90.0f;
+
 const int NUMBER_OF_TEXTURES = 1;
 const GLint LEVEL_OF_DETAIL = 0;
 const GLint TEXTURE_BORDER = 0;
 
 // file path
-const char Mikasa_File_Path[] = ....;
-const char Eren_File_Path[] = ... ;
+const char Mikasa_File_Path[] = "assets/Mikasa.jpeg";
+const char Eren_File_Path[] = "assets/Eren.jpeg";
 
 SDL_Window* g_display_window;
 bool g_game_is_running = true;
 bool g_is_growing = true;
 
 ShaderProgram g_shader_program;
-glm::mat4 view_matrix, g_model_marix, g_projection_matrix, g_trans_matrix;
+glm::mat4 view_matrix, g_model_matrix_1, g_projection_matrix, g_trans_matrix, g_model_matrix_2;
 
 float g_previous_ticks = 0.0f;
 
+const float RADIUS = 2.0f;      // radius of circle
+const float ROT_SPEED = 0.01f;  // rotational speed
+const float ROT_ANGLE = glm::radians(1.5f);
+const float TRAN_VALUE = 0.01f;
 
-Gluint g_mikasa_texture_id;
-Gluint g_eren_texture_id;
+float g_angle    = 0.0f;        // current angle accumulated
+float g_x_coords = RADIUS;      // current x-coord
+float g_y_coords = 0.0f;
+
+const float GROWTH_FACTOR = 1.01f;  // grow by 1.0% / frame
+const float SHRINK_FACTOR = 0.99f;  // grow by -1.0% / frame
+
+
+GLuint g_mikasa_texture_id;
+GLuint g_eren_texture_id;
 
 SDL_Joystick *g_player_one_controller;
 
@@ -124,7 +139,7 @@ void initialise()
         
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
     
-    g_model_marix = glm::mat4(1.0f);
+    g_model_matrix_1 = glm::mat4(1.0f);
     view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
     
@@ -136,8 +151,8 @@ void initialise()
     
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
     
-    g_mikasa_texture_id = ... ;
-    g_eren_texture_id = ... ;
+    g_mikasa_texture_id = load_texture(Mikasa_File_Path) ;
+    g_eren_texture_id = load_texture(Eren_File_Path) ;
     
     // enable blending
     glEnable(GL_BLEND);
@@ -145,66 +160,99 @@ void initialise()
 }
 
 
+
 void process_input()
 {
-    g_player_movement = glm::vec3(0.0f);
-
+    // VERY IMPORTANT: If nothing is pressed, we don't want to go anywhere
     SDL_Event event;
-
-    while (SDL_PollEvent(&event))
-    {
-        switch (event.type) {
-            case SDL_WINDOWEVENT_CLOSE:
-            case SDL_QUIT:
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
+            {
                 g_game_is_running = false;
-                break;
-                
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym) {
-                    case SDLK_RIGHT:
-                        g_player_movement.x = 1.0f;
-                        break;
-                    case SDLK_LEFT:
-                        g_player_movement.x = -1.0f;
-                        break;
-                    case SDLK_q:
-                        g_game_is_running = false;
-                        break;
-                    default:
-                        break;
-                }
-            default:
-                break;
+            }
         }
+
+}
+
+void update()
+{   
+    float ticks = (float) SDL_GetTicks() / MILLISECONDS_IN_SECOND; // get the current number of ticks
+    float delta_time = ticks - g_previous_ticks; // the delta time is the difference from the last frame
+    g_previous_ticks = ticks;
+    
+    
+    g_model_matrix_1 = glm::rotate(g_model_matrix_1, ROT_ANGLE * delta_time, glm::vec3(0.0f, 0.0f, 1.0f));
+    g_model_matrix_1 = glm::translate(g_model_matrix_1, glm::vec3(TRAN_VALUE * delta_time * 10, TRAN_VALUE * delta_time * 10, 0));
+    
+    g_angle += ROT_ANGLE;
+    g_x_coords = RADIUS * glm::cos(g_angle);
+    g_y_coords = RADIUS * glm::sin(g_angle);
+    
+    g_model_matrix_2 = glm::translate(g_model_matrix_1, glm::vec3(g_x_coords * delta_time * 30, g_y_coords * delta_time * 30, 0));
+}
+
+void draw_object(glm::mat4 &object_model_matrix, GLuint &object_texture_id)
+{
+    g_shader_program.set_model_matrix(object_model_matrix);
+    glBindTexture(GL_TEXTURE_2D, object_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so we use 6 instead of 3
+}
+
+
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Vertices
+    float vertices[] = {
+        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
+    };
+
+    // Textures
+    float texture_coordinates[] = {
+        0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
+        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
+    };
+    
+    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(g_shader_program.get_position_attribute());
+    
+    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates);
+    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+    
+    // Bind texture
+    draw_object(g_model_matrix_1, g_mikasa_texture_id);
+    draw_object(g_model_matrix_2, g_eren_texture_id);
+    
+    // We disable two attribute arrays now
+    glDisableVertexAttribArray(g_shader_program.get_position_attribute());
+    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+    
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+
+void shutdown()
+{
+    SDL_JoystickClose(g_player_one_controller);
+    SDL_Quit();
+}
+
+
+int main(int argc, char* argv[])
+{
+    initialise();
+    
+    while (g_game_is_running)
+    {
+        process_input();
+        update();
+        render();
     }
-    const Uint8 *key_states = SDL_GetKeyboardState(NULL); // array of key states [0, 0, 1, 0, 0, ...]
-        
-        if (key_states[SDL_SCANCODE_LEFT])
-        {
-            g_player_movement.x = -1.0f;
-        } else if (key_states[SDL_SCANCODE_RIGHT])
-        {
-            g_player_movement.x = 1.0f;
-        }
-        
-        if (key_states[SDL_SCANCODE_UP])
-        {
-            g_player_movement.y = 1.0f;
-        } else if (key_states[SDL_SCANCODE_DOWN])
-        {
-            g_player_movement.y = -1.0f;
-        }
-        
-        if (glm::length(g_player_movement) > 1.0f)
-        {
-            g_player_movement = glm::normalize(g_player_movement);
-        }
-    }
-
-
-
-
-
-
+    
+    shutdown();
+    return 0;
+}
 
 
