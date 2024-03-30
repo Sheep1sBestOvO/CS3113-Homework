c8b8f63f81a2cec61d1ed6b4c3fa1f3816551164
Zy Yoooung <yoooung@zydemacbook-pro.lan>
2024-03-30 16:14:59 -0400

    finish proj 4

---
+++ week08 exercise/week08 exercise/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  week08 exercise
+//
+//  Created by Zy Yoooung on 3/15/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
